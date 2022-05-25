e6203bbba1ae095be4b6a66ea6c02fd654dbc209
Cihad Atınç <atinccihad@gmail.com>
2022-05-26 01:38:28 +0300

    Q

---
+++ day13_fileExist_fileDownload_acrions/C09_ExpilicitlyWait.java
@@ -0,0 +1,16 @@
+package tests.day13;
+
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+import utilities.TestBase;
+import java.time.Duration;
+
+public class C09_ExpilicitlyWait extends TestBase {
+
+    @Test
+    public void test01(){
+
+        WebDriverWait wait=new WebDriverWait(driver,Duration.ofSeconds(50));
+        //wait.until(ExpectedConditions.);
+    }
+}
