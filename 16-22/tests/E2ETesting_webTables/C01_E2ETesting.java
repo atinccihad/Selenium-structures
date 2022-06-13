204979b0b004db4a8b1a1ab97576e4bbbd6587f3
Cihad Atınç <atinccihad@gmail.com>
2022-06-13 17:43:28 +0300

    Q

---
+++ 16-22/tests/E2ETesting_webTables/C01_E2ETesting.java
@@ -0,0 +1,54 @@
+package tests.day18;
+
+import com.github.javafaker.Faker;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.Test;
+import pages.HotelMyCampPage;
+import utilities.Driver;
+
+// 1. C01_E2ETesting
+public class C01_E2ETesting {
+
+    @Test
+    public void test01(){
+        // 2. Bir method  olusturun: createHotel
+        // 3. https://www.hotelmycamp.com  adresine git.
+        // 4. Username tectbox ve password metin kutularini locate edin ve asagidaki verileri girin.
+        //    a. Username : manager
+        //    b. Password : Manager1!
+        // 5. Login butonuna tiklayin.
+        HotelMyCampPage hotelMyCampPage=new HotelMyCampPage();
+        hotelMyCampPage.girisYap();
+        // 6. Hotel Management/Hotel List menusunden ADD HOTEL butonuna tiklayin
+        hotelMyCampPage.bekle(3);
+        hotelMyCampPage.hotelManagementLinki.click();
+        hotelMyCampPage.hotelListLinki.click();
+        hotelMyCampPage.addHotelLinki.click();
+        // 7. Acilan sayfadaki tum metin kutularina istediginiz verileri girin.
+        Actions actions=new Actions(Driver.getDriver());
+        Faker faker=new Faker();
+        actions.click(hotelMyCampPage.addHotelCodeKutusu)
+                .sendKeys(faker.address().zipCode())
+                .sendKeys(Keys.TAB)
+                .sendKeys(faker.name().name())
+                .sendKeys(Keys.TAB)
+                .sendKeys(faker.address().fullAddress())
+                .sendKeys(Keys.TAB)
+                .sendKeys(faker.phoneNumber().cellPhone())
+                .sendKeys(Keys.TAB)
+                .sendKeys(faker.internet().emailAddress())
+                .sendKeys(Keys.PAGE_DOWN)
+                .perform();
+        Select select=new Select(hotelMyCampPage.addHotelDropdown);
+        select.selectByVisibleText("Hotel Type2");
+
+        // 8. Save butonuna tiklayim.
+        hotelMyCampPage.addHotelSaveButonu.click();
+        hotelMyCampPage.bekle(2);
+
+        // 9. "Hotel was inserted successfully" textinin gorundugunu test edin.
+        // 10. OK butonuna tiklayin.
+    }
+}
