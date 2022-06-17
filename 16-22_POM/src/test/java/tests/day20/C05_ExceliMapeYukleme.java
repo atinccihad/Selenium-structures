8f870b073e7b816936f889de5521923138047c24
Cihad Atınç <atinccihad@gmail.com>
2022-06-17 17:36:37 +0300

    Q

---
+++ 16-22_POM/src/test/java/tests/day20/C05_ExceliMapeYukleme.java
@@ -0,0 +1,28 @@
+package tests.day20;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.Map;
+
+public class C05_ExceliMapeYukleme {
+    @Test
+    public void test01(){
+        // dosya yolu ve sayfa ismi verilen bir excel sheet'i map olarak kaydeden
+        // reusable method olusturalim
+
+        String path="src/test/java/resources/ulkeler.xlsx";
+        String sayfaAdi="Sayfa1";
+
+        System.out.println(ReusableMethodsExcel.mapOlustur(path, sayfaAdi));
+
+        // olusturdugumuz map'i kullanarak Turkey'in bilgilerini yazdirin
+        Map<String,String> ulkelerMap= ReusableMethodsExcel.mapOlustur(path,sayfaAdi);
+        System.out.println(ulkelerMap.get("Turkey"));
+
+        // Listede Netherlands oldugunu test edin
+        Assert.assertTrue(ulkelerMap.containsKey("Netherlands"));
+
+
+    }
+}
