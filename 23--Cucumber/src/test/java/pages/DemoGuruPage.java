648fbc8119b2726963dd1f308aa8698ab764d1ca
Cihad Atınç <atinccihad@gmail.com>
2022-06-21 00:05:35 +0300

    Q

---
+++ 23--Cucumber/src/test/java/pages/DemoGuruPage.java
@@ -0,0 +1,34 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+import java.util.List;
+
+public class DemoGuruPage {
+    public DemoGuruPage() {
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    @FindBy(xpath = "//thead//tr//th")
+    public List<WebElement> baslikWebelementListesi;
+
+    @FindBy(xpath = "//iframe[@class='faktor-iframe-wrapper']")
+    public WebElement iframe;
+
+
+    public List<WebElement> sutunListesiGetir(int index) {
+        //    //tbody//tr//td[3]
+
+        String dinamikXpath = "//tbody//tr//td[" + (index + 1) + "]";
+
+        List<WebElement> istenenSutunWebelementleri = Driver.getDriver().findElements(By.xpath(dinamikXpath));
+
+        return istenenSutunWebelementleri;
+    }
+
+
+}
