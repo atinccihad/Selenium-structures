814b1e8401c51be8c6109dbed0e5f79d16acf214
Cihad Atınç <atinccihad@gmail.com>
2022-06-29 17:39:03 +0300

    Q

---
+++ 08-15_TestNG/src/test/java/tests/practice/Q_09.java
@@ -0,0 +1,34 @@
+package tests.practice;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import utilities.TestBase;
+
+public class Q_09 extends TestBase {
+    // Siteyi aciniz. http://opencart.abstracta.us/index.php?route=account/login ,
+    // login yapiniz Email: batch44@gmail.com  password: 123456789
+    // Search fonksiyonunu kullanarak
+    // Mac, ipod ve samsung icin Dataprovider ile arama yapiniz.
+
+    @Test(dataProvider = "getData")
+    void searchFunction(String txt) {
+        driver.get("http://opencart.abstracta.us/index.php?route=account/login");
+        driver.findElement(By.xpath("//input[@id='input-email']")).sendKeys("batch44@gmail.com");
+        driver.findElement(By.xpath("//input[@id='input-password']")).sendKeys("123456789");
+        driver.findElement(By.xpath("//input[@value='Login']")).click();
+
+        WebElement searchArea = driver.findElement(By.xpath("//input[@name='search']"));
+        searchArea.sendKeys(txt);
+        driver.findElement(By.xpath("//button[@class='btn btn-default btn-lg']")).click();
+    }
+
+    @DataProvider
+    public Object[][] getData() {
+        String data[][] = {{"mac"}, {"ipod"}, {"samsung"}};
+        return data;
+    }
+
+}
